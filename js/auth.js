c64ea0992151a53b64b701bc09323fd953eacd45
harapandarinol <saas.automatic@gmail.com>
2026-03-17 07:13:02 +0700

    Update auth.js

--- js/auth.js
+++ js/auth.js
@@ -1,24 +1,21 @@
-
 async function login(){
+  const email = document.getElementById("email").value;
+  const password = document.getElementById("password").value;
 
-const email=document.getElementById("email").value
-const password=document.getElementById("password").value
+  const res = await fetch(API,{
+    method:"POST",
+    body: JSON.stringify({
+      action:"login",
+      email,
+      password
+    })
+  });
 
-const res=await api({
-action:"login",
-email,
-password
-})
+  const data = await res.json();
 
-if(res.status==="success"){
-
-localStorage.token=res.token
-window.location="dashboard.html"
-
-}else{
-
-alert("Login gagal")
-
-}
-
+  if(data.status === "ok"){
+    window.location = "dashboard.html";
+  }else{
+    alert("Login gagal");
+  }
 }
